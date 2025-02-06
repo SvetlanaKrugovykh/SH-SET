8b7fcdbebac60a6a7281ba5e0b1aa253c0392cb5
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-02-06 14:38:26 +0100

    fixed

--- backup-admin-sh/backupTraffik.sh
+++ backup-admin-sh/backupTraffik.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 pg_dump -U postgres -d traffic -h localhost -p 5432 > traf_$(date +\%Y-\%m-\%d).sql
-tar -rf  traf_$(date +\%Y-\%m-\%d).sql.tar traf_$(date +\%Y-\%m-\%d).sql
+
+tar -czf traf_$(date +\%Y-\%m-\%d).sql.tar.gz traf_$(date +\%Y-\%m-\%d).sql
+rm traf_$(date +\%Y-\%m-\%d).sql
+
